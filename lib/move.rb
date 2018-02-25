59448ef4ae4d7632d343e3567650c4fbb8928191
Tomas Medina <tomas.medina@wework.com>
2018-02-25 18:54:59 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,12 +6,24 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+<<<<<<< HEAD
 def input_to_index(input)
   input = input.to_i - 1
 end
 
 def move(array,index,character="X")
     array[index]=character
+=======
+def input_to_index (input)
+  input = input.to_i - 1
+end
+
+def move (board, index, character)
+  if board [index] == " "
+    board [index] = character
+  else
+    puts "Space already taken!"
+>>>>>>> 840abea67da7cf2d406f0a0348a95694cc870544
 end
 
 # code your input_to_index and move method here!
